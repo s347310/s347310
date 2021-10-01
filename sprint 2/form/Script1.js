13697aa8b6a4d005fb20c13051cca9019e3b64e9
s347310 <89676906+s347310@users.noreply.github.com>
2021-10-01 16:17:16 +0930

    Add files via upload

---
+++ sprint 2/form/Script1.js
@@ -0,0 +1,14 @@
+
+
+    function myfunction() {
+
+        open("HTMLpage1.html");
+
+        <div class="signupbtn">
+            <button onclick="myfunction()" type="submit" class="sumbitbtn">Submit</button>
+
+
+        </div>
+
+
+}
